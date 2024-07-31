1825e400221cfc2ec62909bc256a32b87091d5d1
madhuuu98 <madhuanandhan04@gmail.com>
2024-07-31 14:24:59 +0530

    OddNumbers.java

---
+++ OddNumbers.java
@@ -0,0 +1,12 @@
+public class OddNumbers {
+    public static void main(String[] args) {
+        // Loop through numbers from 1 to 100
+        for (int i = 1; i <= 100; i++) {
+            // Check if the number is odd
+            if (i % 2 != 0) {
+                // Print the odd number
+                System.out.println(i);
+            }
+        }
+    }
+}
